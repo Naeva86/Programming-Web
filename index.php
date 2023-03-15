60aa68d8d625c23af3ee7144a344f27ab69230cc
cloud <cloud@34.124.223.51>
2023-03-15 09:47:05 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Latiham Sesi 1</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        
+        <h3>Latihan Web Programming</h3>
+
+        <script>
+            document.write("Latihan web Programming - Dg JavaScript")
+        </script>
+
+        <br>
+
+        <?php 
+            echo "Latihan Web Programming - Dg PHP";
+        ?>
+    </body>
+</html>
